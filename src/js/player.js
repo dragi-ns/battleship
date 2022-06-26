892557465705015be30259862b02d283c8b48b32
dragi-ns <54716240+dragi-ns@users.noreply.github.com>
2022-06-26 14:43:35 +0200

    Make bot a bit smarter

--- src/js/player.js
+++ src/js/player.js
@@ -1,16 +1,24 @@
-import { getRandomInt } from './util';
+import { areValidCoordinates, getRandomInt } from './util';
 
 export default function Player({ name }) {
   return {
+    isBot: false,
     name,
   };
 }
 
 export function isBot(player) {
-  const moves = [];
+  const playedMoves = [];
+  let hitCount = 0;
+  let initialHitMove = null;
+  let possibleMoves = [];
 
   function noMoreVariations(gridSize) {
-    return gridSize * gridSize <= moves.length;
+    return gridSize * gridSize <= playedMoves.length;
+  }
+
+  function alreadyPlayed(move) {
+    return playedMoves.find(({ x, y }) => x === move.x && y === move.y);
   }
 
   function getRandomMove(gridSize) {
@@ -18,7 +26,7 @@ export function isBot(player) {
       x: getRandomInt(gridSize),
       y: getRandomInt(gridSize),
     };
-    if (!moves.find(({ x, y }) => x === move.x && y === move.y)) {
+    if (!alreadyPlayed(move)) {
       return move;
     }
     return getRandomMove(gridSize);
@@ -28,13 +36,127 @@ export function isBot(player) {
     if (noMoreVariations(gridSize)) {
       return { x: null, y: null };
     }
-    const move = getRandomMove(gridSize);
-    moves.push(move);
+
+    let move;
+    if (possibleMoves.length > 0) {
+      [move] = possibleMoves.splice(getRandomInt(possibleMoves.length), 1);
+    } else {
+      move = getRandomMove(gridSize);
+    }
+    playedMoves.push(move);
     return move;
   }
 
+  function generateUpDirection(move) {
+    return { x: move.x, y: move.y - 1, direction: 'up' };
+  }
+
+  function generateDownDirection(move) {
+    return { x: move.x, y: move.y + 1, direction: 'down' };
+  }
+
+  function generateLeftDirection(move) {
+    return { x: move.x - 1, y: move.y, direction: 'left' };
+  }
+
+  function generateRightDirection(move) {
+    return { x: move.x + 1, y: move.y, direction: 'right' };
+  }
+
+  function generatePossibleMoves(move, gridSize, reset = false) {
+    if (!move.direction) {
+      const up = generateUpDirection(move);
+      if (areValidCoordinates(up, gridSize) && !alreadyPlayed(up)) {
+        possibleMoves.push(up);
+      }
+
+      const down = generateDownDirection(move);
+      if (areValidCoordinates(down, gridSize) && !alreadyPlayed(down)) {
+        possibleMoves.push(down);
+      }
+
+      const left = generateLeftDirection(move);
+      if (areValidCoordinates(left, gridSize) && !alreadyPlayed(left)) {
+        possibleMoves.push(left);
+      }
+
+      const right = generateRightDirection(move);
+      if (areValidCoordinates(right, gridSize) && !alreadyPlayed(right)) {
+        possibleMoves.push(right);
+      }
+
+      return;
+    }
+
+    if (!reset) {
+      if (move.direction === 'up') {
+        const up = generateUpDirection(move);
+        if (areValidCoordinates(up, gridSize) && !alreadyPlayed(up)) {
+          possibleMoves.push(up);
+        }
+      } else if (move.direction === 'down') {
+        const down = generateDownDirection(move);
+        if (areValidCoordinates(down, gridSize) && !alreadyPlayed(down)) {
+          possibleMoves.push(down);
+        }
+      } else if (move.direction === 'left') {
+        const left = generateLeftDirection(move);
+        if (areValidCoordinates(left, gridSize) && !alreadyPlayed(left)) {
+          possibleMoves.push(left);
+        }
+      } else if (move.direction === 'right') {
+        const right = generateRightDirection(move);
+        if (areValidCoordinates(right, gridSize) && !alreadyPlayed(right)) {
+          possibleMoves.push(right);
+        }
+      }
+    }
+
+    if (possibleMoves.length === 0) {
+      if (move.direction === 'up') {
+        possibleMoves.push(generateDownDirection(initialHitMove));
+      } else if (move.direction === 'down') {
+        possibleMoves.push(generateUpDirection(initialHitMove));
+      } else if (move.direction === 'left') {
+        possibleMoves.push(generateRightDirection(initialHitMove));
+      } else if (move.direction === 'right') {
+        possibleMoves.push(generateLeftDirection(initialHitMove));
+      }
+    }
+  }
+
+  function lastMoveFeedback(attack, gridSize) {
+    const lastMove = playedMoves[playedMoves.length - 1];
+    if (attack.status === 'miss') {
+      if (hitCount > 1) {
+        possibleMoves = [];
+        const lastHitMove = playedMoves[playedMoves.length - 2];
+        generatePossibleMoves(lastHitMove, gridSize, true);
+      }
+    } else if (attack.status === 'hit') {
+      hitCount += 1;
+      if (hitCount === 1) {
+        initialHitMove = lastMove;
+      } else {
+        possibleMoves = [];
+      }
+      generatePossibleMoves(lastMove, gridSize);
+    } else if (attack.status === 'sunk') {
+      hitCount = 0;
+      possibleMoves = [];
+      initialHitMove = null;
+      attack.adjacentCoordinates.forEach((coordinate) => {
+        if (!alreadyPlayed(coordinate)) {
+          playedMoves.push(coordinate);
+        }
+      });
+    }
+  }
+
   return {
-    getMove,
     ...player,
+    isBot: true,
+    getMove,
+    lastMoveFeedback,
   };
 }
--- src/js/player.test.js
+++ src/js/player.test.js
@@ -10,6 +10,14 @@ test('player object should have the correct name property value', () => {
   expect(Player({ name: 'Jeff' }).name).toBe('Jeff');
 });
 
+test('player is not a bot', () => {
+  expect(Player({ name: 'Jeff' }).isBot).toBe(false);
+});
+
+test('bot is a bot', () => {
+  expect(pipe(Player, isBot)({ name: 'Jeff' }).isBot).toBe(true);
+});
+
 test('player bot object should have the getMove method', () => {
   const bot = pipe(Player, isBot)({ name: 'Jabotium' });
   expect(bot.hasOwnProperty('getMove')).toBe(true);
@@ -36,4 +44,41 @@ test('player bot getMove method should return null when there is no more variati
   bot.getMove(1);
   const move = bot.getMove(1);
   expect(move).toEqual({ x: null, y: null });
+});
+
+test('player bot should make a smarter decision based on the last move feedback', () => {
+  const bot = pipe(Player, isBot)({ name: 'Jabotium' });
+  const initialHitMove = bot.getMove(5);
+  bot.lastMoveFeedback({ status: 'hit' });
+  bot.getMove(5);
+  bot.lastMoveFeedback({ status: 'hit' });
+  const missMove = bot.getMove(5);
+  bot.lastMoveFeedback({ status: 'miss' });
+  const newMove = bot.getMove(5);
+
+  if (missMove.direction === 'up') {
+    expect(newMove).toEqual({
+      x: initialHitMove.x,
+      y: initialHitMove.y + 1,
+      direction: 'down',
+    });
+  } else if (missMove.direction === 'down') {
+    expect(newMove).toEqual({
+      x: initialHitMove.x,
+      y: initialHitMove.y - 1,
+      direction: 'up',
+    });
+  } else if (missMove.direction === 'left') {
+    expect(newMove).toEqual({
+      x: initialHitMove.x + 1,
+      y: initialHitMove.y,
+      direction: 'right',
+    });
+  } else if (missMove.direction === 'right') {
+    expect(newMove).toEqual({
+      x: initialHitMove.x - 1,
+      y: initialHitMove.y,
+      direction: 'left',
+    });
+  }
 });
