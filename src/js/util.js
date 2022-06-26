2321ee32e0e2d8191d104f8de680ede7a07cb9cc
dragi-ns <54716240+dragi-ns@users.noreply.github.com>
2022-06-26 14:46:39 +0200

    Add util function

--- src/js/util.js
+++ src/js/util.js
@@ -7,14 +7,25 @@ export function getRandomInt(maxInt) {
   return Math.floor(Math.random() * maxInt);
 }
 
+export function getRandomCoordinates(gridSize) {
+  return {
+    x: getRandomInt(gridSize),
+    y: getRandomInt(gridSize),
+  };
+}
+
+export function areValidCoordinates({ x, y }, gridSize) {
+  return x >= 0 && x < gridSize && y >= 0 && y < gridSize;
+}
+
 export function coordinatesToCellNumber({ x, y }, gridSize) {
-  return x * gridSize + y;
+  return y * gridSize + x;
 }
 
 export function cellNumberToCoordinates(cellNumber, gridSize) {
   return {
     // https://stackoverflow.com/a/5494983
-    x: Math.floor(cellNumber / gridSize),
-    y: cellNumber % gridSize,
+    x: cellNumber % gridSize,
+    y: Math.floor(cellNumber / gridSize),
   };
 }
--- src/js/util.test.js
+++ src/js/util.test.js
@@ -1,9 +1,21 @@
-import { coordinatesToCellNumber, cellNumberToCoordinates } from './util';
+import {
+  coordinatesToCellNumber,
+  cellNumberToCoordinates,
+  areValidCoordinates,
+} from './util';
 
 test('coordinatesToCellNumber should correctly convert coordinates to a cell number', () => {
-  expect(coordinatesToCellNumber({ x: 2, y: 5 }, 10)).toBe(25);
+  expect(coordinatesToCellNumber({ x: 5, y: 2 }, 10)).toBe(25);
 });
 
 test('cellNumberToCoordinates should correctly convert a cell number to coordinates', () => {
-  expect(cellNumberToCoordinates(25, 10)).toEqual({ x: 2, y: 5 });
+  expect(cellNumberToCoordinates(25, 10)).toEqual({ x: 5, y: 2 });
+});
+
+test('areValidCoordinates should return true for valid coordinates', () => {
+  expect(areValidCoordinates({ x: 5, y: 5 }, 10)).toBe(true);
+});
+
+test('areValidCoordinates should return false for invalid coordinates', () => {
+  expect(areValidCoordinates({ x: 5, y: 5 }, 5)).toBe(false);
 });
