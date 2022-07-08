a0a921e8ee5aaa1787222649bab048b4a7053c89
dragi-ns <54716240+dragi-ns@users.noreply.github.com>
2022-07-08 14:57:14 +0200

    Clean up gameData

--- src/js/gameData.js
+++ src/js/gameData.js
@@ -1,4 +1,9 @@
 import PubSub from 'pubsub-js';
+import Gameboard, { GRID_SIZE, Status } from './gameboard';
+import Ship from './ship';
+import Player, { isBot } from './player';
+import { pipe } from './util';
+import SHIPS_DATA from '../data/ships.json';
 import {
   START_GAME,
   DISPLAY_SETUP_PHASE,
@@ -11,18 +16,13 @@ import {
   SETUP_PHASE_INCOMPLETE,
   DISPLAY_ATTACK_PHASE,
   ATTACK,
+  MISS,
   HIT,
   SUNK,
-  MISS,
   FLEET_GONE,
   NEW_ROUND,
   RESET_GAME,
 } from './eventTypes';
-import Gameboard, { GRID_SIZE, Status } from './gameboard';
-import Ship from './ship';
-import Player, { isBot } from './player';
-import { pipe } from './util';
-import SHIPS_DATA from '../data/ships.json';
 
 export const GameMode = Object.freeze({
   VS_FRIEND: 0,
@@ -31,24 +31,24 @@ export const GameMode = Object.freeze({
 
 export default function GameData() {
   let state = {
-    mode: null,
+    gameMode: null,
     playerOne: null,
     playerTwo: null,
     availableFleet: null,
-    currentSetupPhasePlayer: null,
+    currentSetupPlayer: null,
     currentAttacker: null,
     currentOpponent: null,
   };
 
   PubSub.subscribe(START_GAME, (_, data) => {
-    state.mode = data.mode;
+    state.gameMode = data.gameMode;
     state.playerOne = {
       ...Player({ name: data.playerOneName }),
       gameboard: Gameboard(),
     };
 
     let playerTwo = null;
-    if (state.mode === GameMode.VS_BOT) {
+    if (state.gameMode === GameMode.VS_BOT) {
       playerTwo = pipe(Player, isBot)({ name: data.playerTwoName });
     } else {
       playerTwo = Player({ name: data.playerTwoName });
@@ -58,12 +58,12 @@ export default function GameData() {
       gameboard: Gameboard(),
     };
 
-    state.currentSetupPhasePlayer = state.playerOne;
-
     state.availableFleet = generateFleet();
+    state.currentSetupPlayer = state.playerOne;
+
     PubSub.publish(DISPLAY_SETUP_PHASE, {
-      playerName: state.currentSetupPhasePlayer.name,
-      gameboard: state.currentSetupPhasePlayer.gameboard.getGrid(),
+      playerName: state.currentSetupPlayer.name,
+      gameboard: state.currentSetupPlayer.gameboard.getGrid(),
       fleet: state.availableFleet,
     });
   });
@@ -73,19 +73,18 @@ export default function GameData() {
     if (!ship) {
       return;
     }
+    const canPlaceShip = state.currentSetupPlayer.gameboard.canPlaceShip(
+      data.startCoords,
+      data.shipOrientation,
+      ship.length,
+      ship.id
+    );
 
-    if (
-      state.currentSetupPhasePlayer.gameboard.canPlaceShip(
-        data.startCoords,
-        data.shipOrientation,
-        ship.length,
-        data.shipId
-      )
-    ) {
+    if (canPlaceShip) {
       PubSub.publish(COORDINATES_FREE, {
         startCoords: data.startCoords,
-        shipLength: ship.length,
         shipOrientation: data.shipOrientation,
+        shipLength: ship.length,
       });
     }
   });
@@ -95,37 +94,37 @@ export default function GameData() {
     if (!ship) {
       return;
     }
-    let success = null;
+
+    let placedShip = null;
     if (data.oldStartCoords) {
-      success = state.currentSetupPhasePlayer.gameboard.moveShip(
+      placedShip = state.currentSetupPlayer.gameboard.moveShip(
         data.oldStartCoords,
         { i: data.startCoords.x, j: data.startCoords.y },
         data.shipOrientation
       );
     } else {
-      success = state.currentSetupPhasePlayer.gameboard.placeShip(
+      placedShip = state.currentSetupPlayer.gameboard.placeShip(
         data.startCoords,
         data.shipOrientation,
         ship
       );
     }
-    if (success) {
+
+    if (placedShip) {
       PubSub.publish(PLACEMENT_VALID, {
         startCoords: data.startCoords,
-        shipLength: ship.length,
         shipOrientation: data.shipOrientation,
+        shipLength: ship.length,
         shipId: ship.id,
       });
     }
   });
 
   PubSub.subscribe(PLACE_SHIPS_RANDOM, () => {
-    state.currentSetupPhasePlayer.gameboard.placeShipsRandom(
-      state.availableFleet
-    );
+    state.currentSetupPlayer.gameboard.placeShipsRandom(state.availableFleet);
     PubSub.publish(DISPLAY_SETUP_PHASE, {
-      playerName: state.currentSetupPhasePlayer.name,
-      gameboard: state.currentSetupPhasePlayer.gameboard.getGrid(),
+      playerName: state.currentSetupPlayer.name,
+      gameboard: state.currentSetupPlayer.gameboard.getGrid(),
       fleet: state.availableFleet,
       random: true,
     });
@@ -133,7 +132,7 @@ export default function GameData() {
 
   PubSub.subscribe(SETUP_PHASE_NEXT, () => {
     if (
-      state.currentSetupPhasePlayer.gameboard.getFleet().length !==
+      state.currentSetupPlayer.gameboard.getFleet().length !==
       state.availableFleet.length
     ) {
       PubSub.publish(SETUP_PHASE_INCOMPLETE);
@@ -141,46 +140,24 @@ export default function GameData() {
     }
 
     state.availableFleet = generateFleet();
-    if (state.currentSetupPhasePlayer === state.playerOne) {
-      if (state.mode === GameMode.VS_FRIEND) {
-        state.currentSetupPhasePlayer = state.playerTwo;
+    if (state.currentSetupPlayer === state.playerOne) {
+      if (state.gameMode === GameMode.VS_FRIEND) {
+        state.currentSetupPlayer = state.playerTwo;
         PubSub.publish(DISPLAY_SETUP_PHASE, {
-          playerName: state.currentSetupPhasePlayer.name,
-          gameboard: state.currentSetupPhasePlayer.gameboard.getGrid(),
+          playerName: state.currentSetupPlayer.name,
+          gameboard: state.currentSetupPlayer.gameboard.getGrid(),
           fleet: state.availableFleet,
         });
         return;
       }
-      state.currentSetupPhasePlayer = null;
+      // In this case playerTwo is a bot
+      state.currentSetupPlayer = null;
       state.playerTwo.gameboard.placeShipsRandom(state.availableFleet);
     }
-    // Randomly pick an attacker and an opponent
-    [state.currentAttacker, state.currentOpponent] = [
-      state.playerOne,
-      state.playerTwo,
-    ].sort(() => (Math.random() > 0.5 ? 1 : -1));
-    state.currentAttacker.setActive(true);
-    state.currentOpponent.setActive(false);
 
-    PubSub.publish(DISPLAY_ATTACK_PHASE, {
-      mode: state.mode,
-      playerOne: {
-        name: state.playerOne.name,
-        isBot: state.playerOne.isBot,
-        active: state.playerOne.isActive(),
-        score: state.playerOne.getScore(),
-        gameboardData: state.playerOne.gameboard.getGrid(),
-        fleet: state.playerOne.gameboard.getFleet(),
-      },
-      playerTwo: {
-        name: state.playerTwo.name,
-        isBot: state.playerTwo.isBot,
-        active: state.playerTwo.isActive(),
-        score: state.playerTwo.getScore(),
-        gameboardData: state.playerTwo.gameboard.getGrid(),
-        fleet: state.playerTwo.gameboard.getFleet(),
-      },
-    });
+    swapRoles();
+
+    PubSub.publish(DISPLAY_ATTACK_PHASE, generateAttackPhaseData());
 
     if (state.currentAttacker.isBot) {
       setTimeout(() => {
@@ -192,52 +169,40 @@ export default function GameData() {
 
   PubSub.subscribe(ATTACK, (_, data) => {
     const attack = state.currentOpponent.gameboard.receiveAttack(data.coords);
+    if (attack === null) {
+      return;
+    }
+
     if (state.currentAttacker.isBot) {
       state.currentAttacker.lastMoveFeedback(attack, GRID_SIZE);
     }
 
     if (attack.status === Status.MISS) {
-      const oldAttacker = state.currentAttacker;
-      state.currentAttacker = state.currentOpponent;
-      state.currentOpponent = oldAttacker;
-      state.currentAttacker.setActive(true);
-      state.currentOpponent.setActive(false);
-
+      swapRoles();
       PubSub.publish(MISS, {
         coords: data.coords,
-        mode: state.mode,
-        playerOne: {
-          name: state.playerOne.name,
-          isBot: state.playerOne.isBot,
-          active: state.playerOne.isActive(),
-          score: state.playerOne.getScore(),
-          gameboardData: state.playerOne.gameboard.getGrid(),
-          fleet: state.playerOne.gameboard.getFleet(),
-        },
-        playerTwo: {
-          name: state.playerTwo.name,
-          isBot: state.playerTwo.isBot,
-          active: state.playerTwo.isActive(),
-          score: state.playerTwo.getScore(),
-          gameboardData: state.playerTwo.gameboard.getGrid(),
-          fleet: state.playerTwo.gameboard.getFleet(),
-        },
+        ...generateAttackPhaseData(),
       });
     } else if (attack.status === Status.HIT) {
       PubSub.publish(HIT, { coords: data.coords });
     } else if (attack.status === Status.SUNK) {
+      const attackerName = state.currentAttacker.name;
+      const opponentName = state.currentOpponent.name;
       PubSub.publish(SUNK, {
         shipId: attack.ship.id,
+        shipName: attack.ship.name,
+        attackerName,
+        opponentName,
         shipCoords: attack.shipCoords,
         adjacentCoords: attack.adjacentCoords,
       });
-
       if (state.currentOpponent.gameboard.isFleetSunk()) {
-        state.currentAttacker.setScore(state.currentAttacker.getScore() + 1);
+        state.currentAttacker.incrementScore();
         PubSub.publish(FLEET_GONE, {
-          winner: state.currentAttacker.name,
-          loser: state.currentOpponent.name,
+          winnerName: attackerName,
+          loserName: opponentName,
         });
+        return;
       }
     }
 
@@ -249,18 +214,6 @@ export default function GameData() {
     }
   });
 
-  PubSub.subscribe(RESET_GAME, () => {
-    state = {
-      mode: null,
-      playerOne: null,
-      playerTwo: null,
-      availableFleet: null,
-      currentSetupPhasePlayer: null,
-      currentAttacker: null,
-      currentOpponent: null,
-    };
-  });
-
   PubSub.subscribe(NEW_ROUND, () => {
     state.playerOne.gameboard = Gameboard();
     state.playerTwo.gameboard = Gameboard();
@@ -268,14 +221,26 @@ export default function GameData() {
       state.playerTwo.clearHistory();
     }
 
-    state.currentSetupPhasePlayer = state.playerOne;
-
     state.availableFleet = generateFleet();
+    state.currentSetupPlayer = state.playerOne;
+
     PubSub.publish(DISPLAY_SETUP_PHASE, {
-      playerName: state.currentSetupPhasePlayer.name,
-      gameboard: state.currentSetupPhasePlayer.gameboard.getGrid(),
+      playerName: state.currentSetupPlayer.name,
+      gameboard: state.currentSetupPlayer.gameboard.getGrid(),
       fleet: state.availableFleet,
     });
+  });
+
+  PubSub.subscribe(RESET_GAME, () => {
+    state = {
+      gameMode: null,
+      playerOne: null,
+      playerTwo: null,
+      availableFleet: null,
+      currentSetupPlayer: null,
+      currentAttacker: null,
+      currentOpponent: null,
+    };
   });
 
   function generateFleet() {
@@ -293,5 +258,43 @@ export default function GameData() {
       return null;
     }
     return state.availableFleet.find((ship) => ship.id === shipId);
+  }
+
+  function swapRoles() {
+    if (state.currentAttacker === null || state.currentOpponent === null) {
+      // Randomly picks an attacker and an opponent
+      [state.currentAttacker, state.currentOpponent] = [
+        state.playerOne,
+        state.playerTwo,
+      ].sort(() => (Math.random() > 0.5 ? 1 : -1));
+    } else {
+      const oldAttacker = state.currentAttacker;
+      state.currentAttacker = state.currentOpponent;
+      state.currentOpponent = oldAttacker;
+    }
+    state.currentAttacker.setActive(true);
+    state.currentOpponent.setActive(false);
+  }
+
+  function generateAttackPhaseData() {
+    return {
+      gameMode: state.gameMode,
+      playerOne: {
+        name: state.playerOne.name,
+        isBot: state.playerOne.isBot,
+        active: state.playerOne.isActive(),
+        score: state.playerOne.getScore(),
+        gameboard: state.playerOne.gameboard.getGrid(),
+        fleet: state.playerOne.gameboard.getFleet(),
+      },
+      playerTwo: {
+        name: state.playerTwo.name,
+        isBot: state.playerTwo.isBot,
+        active: state.playerTwo.isActive(),
+        score: state.playerTwo.getScore(),
+        gameboard: state.playerTwo.gameboard.getGrid(),
+        fleet: state.playerTwo.gameboard.getFleet(),
+      },
+    };
   }
 }
